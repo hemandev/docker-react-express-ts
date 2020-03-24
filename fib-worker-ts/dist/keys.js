7dce8b4f029bc8777390c1a65853fbab02395b8e
hemanditwiz <hemanditwiz@gmail.com>
2020-03-25 01:00:29 +0530

    minor changes

--- fib-worker-ts/dist/keys.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.REDIS_HOST = process.env.REDIS_HOST;
-exports.REDIS_PORT = process.env.REDIS_PORT
-    ? parseInt(process.env.REDIS_PORT)
-    : 8080;
